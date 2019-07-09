17121adf997efa669e187d95fd8c9c9d513eb5cd
Ollie Treend <ollie@treend.com>
2019-07-09 17:06:07 +0100

    Make the form interactive
    I've added functionality to make the feedback form interactive.
    
    I moved the feedback form into its own component, `FeedbackForm`. Its responsibility is to render the feedback form and show/hide questions in response to the feedback given. When a user says the page wasn't useful, it asks further questions for more detailed feedback. Whereas if the page was useful, the form will automatically submit and no further questions are asked. Upon form submit, this component will emit a `submit` event.
    
    The top-level `App` component listens for the `submit` event from `FeedbackForm`. Eventually it will save the submitted feedback to a database, however for now it simply emulates a save with a 500ms timeout. Once saved, a 'thank you' message is shown to the user.
    
    I also made the static HTML markup more dynamic:
    - The options under 'Why isn't this page useful?' are dynamic, using `v-for`
    - `v-model` is used to capture the input values
    - The submit button submits the form

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,40 @@
 <template>
-  <div id="app">
-
-
-    <div class="govuk-width-container">
-      <main class="govuk-main-wrapper">
-
-        <hr class="govuk-section-break govuk-section-break--xl govuk-section-break--visible">
-
-        <div class="govuk-form-group">
-          <fieldset class="govuk-fieldset">
-            <legend class="govuk-fieldset__legend govuk-fieldset__legend--m">
-              <h1 class="govuk-fieldset__heading">
-                Is this page useful?
-              </h1>
-            </legend>
-            <div class="govuk-radios govuk-radios--inline govuk-radios--small">
-              <div class="govuk-radios__item">
-                <input class="govuk-radios__input" id="changed-name-1" name="changed-name" type="radio" value="yes">
-                <label class="govuk-label govuk-radios__label" for="changed-name-1">
-                  Yes
-                </label>
-              </div>
-              <div class="govuk-radios__item">
-                <input class="govuk-radios__input" id="changed-name-2" name="changed-name" type="radio" value="no">
-                <label class="govuk-label govuk-radios__label" for="changed-name-2">
-                  No
-                </label>
-              </div>
-            </div>
-          </fieldset>
-        </div>
-
-        <div class="govuk-form-group">
-          <fieldset class="govuk-fieldset">
-            <legend class="govuk-fieldset__legend govuk-fieldset__legend--m">
-              <h1 class="govuk-fieldset__heading">
-                Why isn’t this page useful?
-              </h1>
-            </legend>
-            <div class="govuk-radios govuk-radios--small">
-              <div class="govuk-radios__item">
-                <input class="govuk-radios__input" id="changed-name-12" name="changed-name2" type="radio" value="yes">
-                <label class="govuk-label govuk-radios__label" for="changed-name-12">
-                  It isn’t relevant to my situation
-                </label>
-              </div>
-              <div class="govuk-radios__item">
-                <input class="govuk-radios__input" id="changed-name-23" name="changed-name2" type="radio" value="no">
-                <label class="govuk-label govuk-radios__label" for="changed-name-23">
-                  It doesn’t have enough detail
-                </label>
-              </div>
-              <div class="govuk-radios__item">
-                <input class="govuk-radios__input" id="changed-name-24" name="changed-name2" type="radio" value="no">
-                <label class="govuk-label govuk-radios__label" for="changed-name-24">
-                  I can’t work out what I should do next
-                </label>
-              </div>
-              <div class="govuk-radios__item">
-                <input class="govuk-radios__input" id="changed-name-25" name="changed-name2" type="radio" value="no">
-                <label class="govuk-label govuk-radios__label" for="changed-name-25">
-                  I don’t understand
-                </label>
-              </div>
-            </div>
-          </fieldset>
-        </div>
-
-        <div class="govuk-form-group">
-          <label class="govuk-fieldset__legend govuk-fieldset__legend--m" for="more-detail">
-            Is there anything else you’d like to tell us?
-          </label>
-          <textarea class="govuk-textarea" id="more-detail" name="more-detail" rows="5" aria-describedby="more-detail-hint"></textarea>
-        </div>
-
-        <button type="submit" class="govuk-button">
-          Submit feedback
-        </button>
-
-      </main>
+  <div class="govuk-width-container">
+    <hr class="govuk-section-break govuk-section-break--xl govuk-section-break--visible">
+    <FeedbackForm v-if="submitted === false" @submit="saveFeedback" />
+    <div v-else>
+      <div class="govuk-fieldset__legend govuk-fieldset__legend--m">
+        Is this page useful?
+      </div>
+      <p>Thank you for your feedback.</p>
     </div>
-
-
   </div>
 </template>
 
 <script>
+  import FeedbackForm from '@/components/FeedbackForm';
 
-export default {
-  components: {
+  export default {
+    components: {
+      FeedbackForm,
+    },
+    data() {
+      return {
+        submitted: false,
+      };
+    },
+    methods: {
+      saveFeedback(feedback) {
+        setTimeout(() => {
+          this.submitted = true;
+        }, 500);
+      }
+    },
   }
-}
 </script>
 
 <style lang="scss">
+  $govuk-global-styles: true;
   $govuk-assets-path: "../node_modules/govuk-frontend/assets/";
   $govuk-font-family: "Helvetica Neue",Helvetica,Arial,"Lucida Grande",sans-serif;
   @import "../node_modules/govuk-frontend/core/all";
---
+++ src/components/FeedbackForm.vue
@@ -0,0 +1,95 @@
+<template>
+  <form @submit.prevent="submit">
+    <div class="govuk-form-group">
+      <fieldset class="govuk-fieldset">
+        <legend class="govuk-fieldset__legend govuk-fieldset__legend--m">
+          Is this page useful?
+        </legend>
+        <div class="govuk-radios govuk-radios--inline govuk-radios--small">
+          <div class="govuk-radios__item">
+            <input v-model="useful" :value="true" id="useful-yes" class="govuk-radios__input" type="radio">
+            <label class="govuk-label govuk-radios__label" for="useful-yes">
+              Yes
+            </label>
+          </div>
+          <div class="govuk-radios__item">
+            <input v-model="useful" :value="false" id="useful-no" class="govuk-radios__input" type="radio">
+            <label class="govuk-label govuk-radios__label" for="useful-no">
+              No
+            </label>
+          </div>
+        </div>
+      </fieldset>
+    </div>
+
+    <div v-if="useful === false">
+
+      <div class="govuk-form-group">
+        <fieldset class="govuk-fieldset">
+          <legend class="govuk-fieldset__legend govuk-fieldset__legend--m">
+            Why isn’t this page useful?
+          </legend>
+          <div class="govuk-radios govuk-radios--small">
+            <div v-for="(option, index) in reasonOptions" :key="option" class="govuk-radios__item">
+              <input v-model="reason" :value="option" :id="`reason-${index}`" class="govuk-radios__input" type="radio">
+              <label class="govuk-label govuk-radios__label" :for="`reason-${index}`">
+                {{ option }}
+              </label>
+            </div>
+          </div>
+        </fieldset>
+      </div>
+
+      <div class="govuk-form-group">
+        <label class="govuk-fieldset__legend govuk-fieldset__legend--m" for="comments">
+          Is there anything else you’d like to tell us?
+        </label>
+        <textarea v-model="comments" class="govuk-textarea" id="comments" rows="5"></textarea>
+      </div>
+
+      <button type="submit" class="govuk-button">
+        Submit feedback
+      </button>
+
+    </div>
+  </form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        useful: null,
+        reasonOptions: [
+          'It isn’t relevant to my situation',
+          'It doesn’t have enough detail',
+          'I can’t work out what I should do next',
+          'I don’t understand',
+        ],
+        reason: null,
+        comments: '',
+      };
+    },
+    methods: {
+      submit() {
+        const feedback = {
+          useful: this.useful,
+        };
+
+        if (this.useful === false) {
+          feedback.reason = this.reason;
+          feedback.comments = this.comments;
+        }
+
+        this.$emit('submit', feedback);
+      }
+    },
+    watch: {
+      useful(value) {
+        if (value === true) {
+          this.submit();
+        }
+      }
+    }
+  }
+</script>
